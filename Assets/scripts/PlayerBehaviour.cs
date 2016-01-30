c5556f6ef9df5519666d87e1eea9c012b8805d3c
blufiro <blufiro@gmail.com>
2016-01-30 10:16:24 -0800

    Adding in initial networked player

---
+++ Assets/scripts/PlayerBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class PlayerBehaviour : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!isLocalPlayer)
+            return;
+
+		var x = Input.GetAxis("Horizontal");
+		var y = Input.GetAxis("Vertical");
+		transform.Translate(x, y, 0);
+	}
+
+	public override void OnStartLocalPlayer()
+    {
+        GetComponent<SpriteRenderer>().material.color = Color.red;
+    }
+}
