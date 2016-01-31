09b286823812b266ad5aeb26d83ae49ea7bd3660
blufiro <blufiro@gmail.com>
2016-01-30 19:03:59 -0800

    Fixed brewery spawn positions colliding.

--- Assets/scripts/CiderManager.cs
+++ Assets/scripts/CiderManager.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.Networking;
-using System.Collections;
+using System.Collections.Generic;
 
 public class CiderManager : NetworkBehaviour {
 
@@ -11,22 +11,51 @@ public class CiderManager : NetworkBehaviour {
 
 	public override void OnStartServer()
     {
+		var prevPositions = new List<Vector3>();
+
     	Texture breweryTexture = breweryPrefab.GetComponent<SpriteRenderer>().sprite.texture;
 		int breweryWidth = breweryTexture.width;
 		int breweryHeight = breweryTexture.height;
 
 		for (int i=0; i < numBreweries; i++)
         {
-            var pos = new Vector3(
-				Random.Range(-G.HALF_WORLD_WIDTH + breweryWidth, G.HALF_WORLD_WIDTH - breweryWidth),
-				Random.Range(-G.HALF_WORLD_HEIGHT + breweryHeight, G.HALF_WORLD_HEIGHT - breweryHeight),
-				0.0f);
+            Vector3 pos;
+			int attempts = 0;
+            do {
+	            pos = new Vector3(
+					randPosNeg(Random.Range(G.HALF_SCREEN_WIDTH, G.HALF_WORLD_WIDTH - breweryWidth)),
+					randPosNeg(Random.Range(G.HALF_SCREEN_HEIGHT, G.HALF_WORLD_HEIGHT - breweryHeight)),
+					0.0f);
+
+				Debug.Log("brewery pos: " + pos + " bw " + breweryWidth + " bh "+ breweryHeight);
+			} while (collidesWithPrevious(prevPositions, pos, breweryWidth, breweryHeight)
+				&& attempts++ < G.get().MAX_SPAWN_ATTEMPTS);
+			if (attempts >= G.get().MAX_SPAWN_ATTEMPTS) {
+				Debug.Log("Giving up spawning more brewerys after " + attempts + " attempts");
+				return;
+			}
 
             var rotation = Quaternion.identity; //Euler( Random.Range(0,180), Random.Range(0,180), Random.Range(0,180));
 
 			var brewery = (GameObject)Instantiate(breweryPrefab, pos, rotation);
 			brewery.transform.parent = world.transform;
 			NetworkServer.Spawn(brewery);
+			prevPositions.Add(pos);
         }
     }
+
+    private float randPosNeg(float value) {
+		return (Random.value < 0.5) ? -value : value;
+    }
+
+	private bool collidesWithPrevious(List<Vector3> prevPositions, Vector3 newPos, int w, int h) {
+		foreach (Vector3 prevPos in prevPositions) {
+			Vector3 delta = prevPos - newPos;
+			if (Mathf.Abs(delta.x) < w && Mathf.Abs(delta.y) < h) {
+				Debug.Log("Collides with another brewery!" + prevPos + " "+ newPos + " "+ delta);
+				return true;
+			}
+		}
+		return false;
+	}
 }
--- Assets/scripts/G.cs
+++ Assets/scripts/G.cs
@@ -15,6 +15,7 @@ public class G
 	public float ARROW_LIFE = 1.0f; // seconds
 	public int ARROW_DAMAGE = 1;
 	public float PLAYER_MOVE_SPEED = 100.0f/60.0f; // pixels per second
+	public int MAX_SPAWN_ATTEMPTS = 5;
 
 	private static G instance = new G();
 	public static G get() {
