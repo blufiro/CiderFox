04b033de9d6990fb93d3c1daf810f78a7a7176e9
blufiro <blufiro@gmail.com>
2016-01-30 17:09:41 -0800

    Add breweries spawning

---
+++ Assets/scripts/CiderManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class CiderManager : NetworkBehaviour {
+
+	public GameObject world;
+	public GameObject breweryPrefab;
+	public GameObject ciderPrefab;
+    public int numBreweries;
+
+	public override void OnStartServer()
+    {
+    	Texture breweryTexture = breweryPrefab.GetComponent<SpriteRenderer>().sprite.texture;
+		int breweryWidth = breweryTexture.width;
+		int breweryHeight = breweryTexture.height;
+
+		for (int i=0; i < numBreweries; i++)
+        {
+            var pos = new Vector3(
+				Random.Range(-G.HALF_WORLD_WIDTH + breweryWidth, G.HALF_WORLD_WIDTH - breweryWidth),
+				Random.Range(-G.HALF_WORLD_HEIGHT + breweryHeight, G.HALF_WORLD_HEIGHT - breweryHeight),
+				0.0f);
+
+            var rotation = Quaternion.identity; //Euler( Random.Range(0,180), Random.Range(0,180), Random.Range(0,180));
+
+			var brewery = (GameObject)Instantiate(breweryPrefab, pos, rotation);
+			brewery.transform.parent = world.transform;
+			NetworkServer.Spawn(brewery);
+        }
+    }
+}
