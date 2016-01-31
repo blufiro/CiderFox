48eca182936379c2f9b16a2a68eb6a737c8be062
blufiro <blufiro@gmail.com>
2016-01-31 12:57:32 -0800

    added title, credits and lobby menu

---
+++ Assets/scripts/TitleScreen.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class TitleScreen : MonoBehaviour {
+
+	public GameObject titleMenu;
+	public GameObject lobbyMenu;
+	public GameObject creditsMenu;
+
+	public GameObject networkManager;
+
+	void Start() {
+		switchState(State.TITLE);
+	}
+
+	public void OnClickStart() {
+		switchState(State.LOBBY);
+	}
+
+	public void OnClickCredits() {
+		switchState(State.CREDITS);
+	}
+
+	public void OnClickQuit() {
+		Application.Quit();
+	}
+
+	public void OnClickBackToTitle() {
+		switchState(State.TITLE);
+	}
+
+	public void OnClickHost() {
+		networkManager.GetComponent<NetworkManager>().StartHost();
+	}
+
+	public void OnClickClient() {
+		networkManager.GetComponent<NetworkManager>().StartClient();
+	}
+
+	enum State {
+		TITLE,
+		LOBBY,
+		CREDITS,
+	}
+	private void switchState(State state) {
+		titleMenu.SetActive(false);
+		lobbyMenu.SetActive(false);
+		creditsMenu.SetActive(false);
+		switch (state) {
+			case State.TITLE: titleMenu.SetActive(true); break;
+			case State.LOBBY: lobbyMenu.SetActive(true); break;
+			case State.CREDITS: creditsMenu.SetActive(true); break;
+		}
+	}
+}
