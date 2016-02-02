88b67d2313f34517fee441b6e36cc5fdc90fc962
blufiro <blufiro@gmail.com>
2016-02-01 22:33:50 -0800

    changed angry duration back to 3mins

--- Assets/scripts/G.cs
+++ Assets/scripts/G.cs
@@ -19,7 +19,7 @@ public class G
 	public float LIGHT_OFF_DELAY = 5.0f; //seconds
 	public float CIDER_PRODUCE_DELAY = 30.0f; // seconds
 	public int CARRY_OVERHEAD_OFFSET_Y = 4; // pixels
-	public float GOD_ANGRY_DURATION = 10; // seconds
+	public float GOD_ANGRY_DURATION = 3 * 60; // seconds
 	public float SNAP_TIME_THRESHOLD = 1; // seconds
 
 	private static G instance = new G();
