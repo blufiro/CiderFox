b22b388d782356ebba5dbb06d037e9b9d6b4e583
blufiro <blufiro@gmail.com>
2016-02-03 00:01:23 -0800

    adding matchmaker ui and changing to lobby manager

--- Assets/scripts/TitleScreen.cs
+++ Assets/scripts/TitleScreen.cs
@@ -1,21 +1,63 @@
 ﻿using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.Networking.Match;
 using System.Collections;
 
 public class TitleScreen : MonoBehaviour {
 
 	public GameObject titleMenu;
-	public GameObject lobbyMenu;
+	public GameObject networkMenu;
+	public GameObject lanMenu;
+	public GameObject onlineLobbyMenu;
 	public GameObject creditsMenu;
 
-	public GameObject networkManager;
+	public NetworkLobbyManager networkLobbyManager;
+
+	private NetworkMatch match;
 
 	void Start() {
 		switchState(State.TITLE);
 	}
 
 	public void OnClickStart() {
-		switchState(State.LOBBY);
+		switchState(State.NETWORK);
+	}
+
+	public void OnClickLAN() {
+		switchState(State.LAN);
+	}
+
+	public void OnClickLANHost() {
+		Debug.LogError("TODO");
+	}
+
+	public void OnClickLANClient() {
+		Debug.LogError("TODO");
+	}
+
+	public void OnClickOnline() {
+		switchState(State.ONLINE);
+		networkLobbyManager.StartMatchMaker();
+		match = networkLobbyManager.matchMaker;
+	}
+
+	public void OnClickCreateGame() {
+		var req = new CreateMatchRequest();
+		req.name = "name";
+		req.size = 4;
+		req.advertise = true;
+		req.password = "";
+		match.CreateMatch(req, networkLobbyManager.OnMatchCreate);
+	}
+
+	public void OnClickFindGame() {
+		var req = new ListMatchRequest();
+		match.ListMatches(req, networkLobbyManager.OnMatchList);
+	}
+
+	public void OnClickJoinGame() {
+		var req = new JoinMatchRequest();
+		match.JoinMatch(req, networkLobbyManager.OnMatchJoined);
 	}
 
 	public void OnClickCredits() {
@@ -28,24 +70,30 @@ public class TitleScreen : MonoBehaviour {
 
 	public void OnClickBackToTitle() {
 		switchState(State.TITLE);
+		if (match != null) {
+			networkLobbyManager.StopMatchMaker();
+			match = null;
+		}
 	}
 
 	enum State {
 		TITLE,
-		LOBBY,
+		NETWORK, // LAN or Online
+		LAN, // Host or Client
+		ONLINE, // Create game or Find game
 		CREDITS,
 	}
 	private void switchState(State state) {
 		titleMenu.SetActive(false);
-		lobbyMenu.SetActive(false);
+		networkMenu.SetActive(false);
+		lanMenu.SetActive(false);
+		onlineLobbyMenu.SetActive(false);
 		creditsMenu.SetActive(false);
-		networkManager.GetComponent<NetworkManagerHUD>().showGUI = false;
 		switch (state) {
 			case State.TITLE: titleMenu.SetActive(true); break;
-			case State.LOBBY:
-				lobbyMenu.SetActive(true);
-				networkManager.GetComponent<NetworkManagerHUD>().showGUI = true;
-				break;
+			case State.NETWORK: networkMenu.SetActive(true); break;
+			case State.LAN: lanMenu.SetActive(true); break;
+			case State.ONLINE: onlineLobbyMenu.SetActive(true); break;
 			case State.CREDITS: creditsMenu.SetActive(true); break;
 		}
 	}
